e62d9f33ec97f24c55474f61f3ce014069bc4993
Ran Nyman <ran@rannicon.com>
2018-08-24 13:51:49 +0300

    Added functionality on working with configurations

--- EcuComApi.py
+++ EcuComApi.py
@@ -15,6 +15,7 @@ class EcuComApi:
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
     def run_test_package(self, packageName, timeOutSeonds=30):
+        self.etEnv = self.etApp.Start()
         pkg = self.etApp.OpenPackage(packageName)
         
         if pkg is None:
@@ -32,9 +33,21 @@ class EcuComApi:
         info("ECU test run compelted with result '%s'", status)
         return status
 
+    def open_test_configuration(self, testConfigurationName):
+        debug("Opening test configuration '%s'", testConfigurationName)
+        if not True == self.etApp.OpenTestConfiguration(testConfigurationName):
+            raise RuntimeError("Can not opent test configuration '%s'", testConfigurationName)
+
+    def start_test_environment(self):
+        self.etEnv = self.etApp.Start()
+
     def close_test_package(self, packageName):
         self.etApp.ClosePackage(packageName)
 
+    def stop_test_environment(self):
+        self.etEnv = self.etApp.Stop()
+        self.etEnv = None
+
     def __init__(self):
         self.etApp = win32com.client.Dispatch('ECU-TEST.Application')
-        self.etEnv = self.etApp.Start()
+        self.etEnv = None
