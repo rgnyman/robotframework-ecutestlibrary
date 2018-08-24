0f12e6114e73bf0f9e96a6c3ced5735c18a6f93d
Ran Nyman <ran@rannicon.com>
2018-08-24 09:58:16 +0300

    Cleaning running keywords

--- EcuApiClient.py
+++ EcuApiClient.py
@@ -2,10 +2,10 @@
 
 TODO Add documentation
 
+Copyright 2018 Rannicon Oy All Rights Reserved 
+
+TODO Add License
 """
-
-"""from ApiClient import ConfigurationApi"""
-
 from ApiClient import ApiClient as ApiClientClass
 import ApiClient as ApiClientModule
 from logging import debug, error, info, warn
@@ -24,10 +24,10 @@ class EcuApiClient:
         for c in classes:
             class_methods = dir(c[1])
             class_methods = self.remove_not_needed_methods(class_methods)
-            self.append_class_name(c[0], class_methods, c[1])
+            self.update_keyword_dictionary(c[0], class_methods, c[1])
         return self.keywordMapping
 
-    def append_class_name(self, class_name, methods, object_id):
+    def update_keyword_dictionary(self, class_name, methods, object_id):
         for mn in methods:
             mn = class_name + "." + mn
             self.keywordMapping[mn] = object_id
@@ -45,10 +45,10 @@ class EcuApiClient:
         
     def initialize_method_mapping(self):
         self.keywordMapping = self.get_all_api_methods()
-        self.add_main_api_classes(self.api.ConfigurationApi)
-        self.add_main_api_classes(self.api.PackageApi)
+        self.add_main_api_class(self.api.ConfigurationApi)
+        self.add_main_api_class(self.api.PackageApi)
         
-    def add_main_api_classes(self, api_class):
+    def add_main_api_class(self, api_class):
         members = self.get_method_names(api_class)
         self.add_api_objects(api_class.__class__.__name__, members)
 
@@ -67,19 +67,30 @@ class EcuApiClient:
 
     def run_keyword(self, name, args, kwargs):
         method = self.keywordMapping[name]
-        debug(name)
-        debug(method)
-        if len(args) == 0:
-            if callable(method):
-                ret = method()
-            else:
-                ret = method
+        debug("calling method '%s' found with name '%s' args '%s' kwargs '%s'", method, name, args, kwargs)
+        call_result = self.call_method(method, args, kwargs)
+        debug("call_result '%s'", call_result)
+        object_methods = self.get_method_names(call_result)
+        self.update_method_dictionary(object_methods, call_result.__class__.__name__)
+        return call_result
+
+    def call_method(self, method, args, kwargs):
+        if len(args) == 0 and len(kwargs) == 0:
+            return self.property_or_method_call(method)
         else:
-            ret = method(args[0])
-        """if not ret in args:""" 
-        object_methods = self.get_method_names(ret)
-        self.update_method_dictionary(object_methods, ret.__class__.__name__)
-        return ret
+            return self.args_kwargs_call(method, args, kwargs)
+
+    def property_or_method_call(self, method):
+        if callable(method):
+            return method()
+        else:
+            return method
+
+    def args_kwargs_call(self, method, args, kwargs):
+        if len(kwargs) == 0:
+            return method(*args)
+        else:
+            return method(*args, **kwargs)
 
     def get_keyword_documentation(self, name):
         pass
@@ -91,11 +102,22 @@ class EcuApiClient:
         pass
 
     def update_method_dictionary(self, members, object_name):
+        if object_name == 'NoneType':
+            return
+        members = self.clean_methods(members)
+        self.add_new_keywords(members, object_name)
+    
+    def clean_methods(self, members):
+        members = [elem for elem in members if not (elem[0].isupper())]
+        members = [elem for elem in members if not (elem[0][0] == "_")]
+        return members
+
+    def add_new_keywords(self, members, object_name):
         for m in members:
             name = object_name + '.' + m[0]
-            debug(name, m[1])
+            debug("Adding keyword with name '%s' for method '%s'", name, m[1])
             self.keywordMapping[name] = m[1]
-            
+
     def get_mappings(self):
         return self.keywordMapping
 
